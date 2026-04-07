197caf3d48291cbecff75aef2c1ff186aa5d6a16
Anmol <anmolrajput1601@gmail.com>
2026-04-08 02:00:05 +0530

    added main entrypoint for server

--- server/app.py
+++ server/app.py
@@ -34,3 +34,11 @@ def step(action: Action):
 def get_state():
     global env
     return env.state().__dict__
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
